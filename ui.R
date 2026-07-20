b5336aa5d09ad44d571c5e472af3577b1728eac7
jeanssresende <jean.resende@professor.fpp.edu.br>
2026-07-20 13:20:28 -0300

    feat: bootstrap initial Shiny application

---
+++ ui.R
@@ -0,0 +1,13 @@
+ui <- fluidPage(
+  
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "flatly"
+  ),
+  
+  titlePanel("RNASeqFlow"),
+  
+  h2("Bem-vindo ao RNASeqFlow"),
+  
+  p("Primeira versão do software.")
+)
